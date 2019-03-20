72887ecf03fc92899f3d861318157578a477ec91
uhocaogl <umut.hocaolgu@meredith.com>
2019-03-19 22:29:10 -0500

    Add radio btns for hist graph

--- expenses/helpers/helper.py
+++ expenses/helpers/helper.py
@@ -1,7 +1,7 @@
 import datetime
 
 
-def get_yty_data(db, table, session):
-    return db.session.query(table).filter(table.due_date >= (datetime.datetime.now() - datetime.timedelta(days=365)).replace(day=1),
+def get_six_months_data(db, table, session):
+    return db.session.query(table).filter(table.due_date >= (datetime.datetime.now() - datetime.timedelta(days=184)).replace(day=1),
                                             table.user_id == session['user_id']
-                                            )
+                                            ).order_by(table.due_date.desc()).all()
